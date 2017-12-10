4e0a459c1a35295789424a12b560c4d7e94b0b57
Marcus Perez <MarcusPerez@outlook.com>
2017-12-09 21:26:35 -0500

    Refactor Time To Military Pay Rate for Start To Bedtime Added

--- src/com/babysitter/BabySitter.java
+++ src/com/babysitter/BabySitter.java
@@ -1,34 +1,33 @@
 package com.babysitter;
 
-
-
 public class BabySitter {
 
 	public int startTime;
 	public int endTime;
+	public int bedTime;
+	public int payRateStartToBedtime = 12;
+	
 	public String StartOrEndTimeErrorMessage;
 
 	public BabySitter() {
-		this.startTime = 17;
-		this.endTime = 4;
+		this.startTime = 1700;
+		this.endTime = 400;
+		this.bedTime = 2400;
 	}
 
-	
-
-	public BabySitter(int startTime, int endTime) {
-		if( startTime < 17 || startTime > 4) {
-			StartOrEndTimeErrorMessage ="I can only babysit between 5pm and 4am.";
-			
-		}
+	public BabySitter(int startTime, int endTime, int bedTime) {
+		getStartOrEndTimeErrorMessage();
 		this.startTime = startTime;
 		this.endTime = endTime;
+		this.bedTime = bedTime;
 	}
 
 	public String getStartOrEndTimeErrorMessage() {
+		if (startTime < 1700 || startTime > 400 || endTime < 1700 || endTime > 400) {
+			StartOrEndTimeErrorMessage = "I can only babysit between 5pm and 4am.";
+		}
 		return StartOrEndTimeErrorMessage;
 	}
-
-
 
 	public int getStartTime() {
 		return startTime;
@@ -36,7 +35,7 @@ public class BabySitter {
 
 	public void setStartTime(int startTime) {
 		this.startTime = startTime;
-	
+
 	}
 
 	public int getEndTime() {
@@ -47,5 +46,22 @@ public class BabySitter {
 		this.endTime = endTime;
 	}
 
+	public int getBedTime() {
+		return bedTime;
+	}
+
+	public void setBedTime(int bedTime) {
+		this.bedTime = bedTime;
+	}
+
+	public int calculatePayment() {
+		
+		
+		return payRateStartToBedtime * (bedTime - startTime)/(100);
+		
+		
+	}
+
+	
 
 }
--- src/com/babysitter/BabySitterTest.java
+++ src/com/babysitter/BabySitterTest.java
@@ -1,6 +1,7 @@
 package com.babysitter;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.jupiter.api.Assertions;
 import  org.junit.jupiter.api.Assertions.*;
@@ -19,16 +20,16 @@ class BabySitterTest {
 	public void babySitterStartTimeReturns17() {
 		BabySitter babySitter = new BabySitter();
 
-		assertEquals(17, babySitter.getStartTime());
+		assertEquals(1700, babySitter.getStartTime());
 	}
 
 	@Test
 
 	public void babySitterStartTimeReturnsNewStartTime() {
 		BabySitter babySitter = new BabySitter();
-		babySitter.setStartTime(18);
+		babySitter.setStartTime(1800);
 
-		assertEquals(18, babySitter.getStartTime());
+		assertEquals(1800, babySitter.getStartTime());
 
 	}
 
@@ -36,23 +37,35 @@ class BabySitterTest {
 
 	public void babySitterLatestEndTime() {
 		BabySitter babySitter = new BabySitter();
-		assertEquals(4, babySitter.getEndTime());
+		assertEquals(400, babySitter.getEndTime());
 
 	}
 	
 	@Test
 	public void babySitterEndTimeReturnsNewStartAndEndTime() {
-		BabySitter babySitter = new BabySitter(18 , 3);
-		assertEquals(18, babySitter.getStartTime());
-		assertEquals(3, babySitter.getEndTime());
+		BabySitter babySitter = new BabySitter();
+		babySitter.setStartTime(1800);
+		babySitter.setEndTime(300);
+		assertEquals(1800, babySitter.getStartTime());
+		assertEquals(300, babySitter.getEndTime());
 		
 	}
 	@Test()
 	public void babySitterInvalidStartTimeWithMessage() {
-		BabySitter babySitter = new BabySitter(18,3);
+		BabySitter babySitter = new BabySitter();
+		babySitter.setStartTime(1600);
+		babySitter.setEndTime(400);
 		String InvalidString = "I can only babysit between 5pm and 4am.";
-		equals(InvalidString.equals(babySitter.getStartOrEndTimeErrorMessage()));
+		assertTrue(InvalidString.equals(babySitter.getStartOrEndTimeErrorMessage()));
 		
 	}
-	
+	@Test()
+	public void PayRateFromStartToBedTime12$() {
+		BabySitter babySitter = new BabySitter();
+		babySitter.setStartTime(1700);
+		babySitter.setBedTime(2300);
+		babySitter.setEndTime(2400);
+		;
+		assertEquals( 72 , babySitter.calculatePayment());
+	}
 }
