3e38f5ca6dac4e134987fbf329c2ebf0cb818cf3
Marcus Perez <MarcusPerez@outlook.com>
2017-12-17 19:35:31 -0500

    More refactoring for readability

--- src/main/PaymentRates.java
+++ src/main/PaymentRates.java
@@ -10,9 +10,10 @@ public class PaymentRates {
 	private final int LATEST_END_TIME = 400;
 
 	public int calculatePayment(BabySitter babySitter) {
+		boolean endTimeBeforeLatestEndTime = babySitter.getEndTime() > LATEST_END_TIME;
 		int paymentReturnValue = calculatePaymentForHoursWithAnEndTimeAfterMidnight(babySitter);
 
-		if (babySitter.getEndTime() > LATEST_END_TIME) {
+		if (endTimeBeforeLatestEndTime) {
 			paymentReturnValue = calculatePaymentForHoursWithAnEndTimeBeforeMidnight(babySitter);
 		}
 		return paymentReturnValue;
@@ -23,28 +24,31 @@ public class PaymentRates {
 				/ CONVERSION_24_HOUR_TIME;
 		int paymentForHoursFromBedTimeToMidnight = PAY_RATE_BEDTIME_TO_MIDNIGHT
 				* convertFor24HourTime(MIDNIGHT, babySitter.getBedTime());
+		int totalPaymentForHoursFromStartTillAfterMidnight = calculatePaymentFromStartToBedTime(babySitter)
+				+ paymentForHoursFromBedTimeToMidnight + paymentForHoursFromMidnightToEnd;
 
-		return calculatePaymentFromStartToBedTime(babySitter) + paymentForHoursFromBedTimeToMidnight
-				+ paymentForHoursFromMidnightToEnd;
+		return totalPaymentForHoursFromStartTillAfterMidnight;
+
 	}
 
 	private int calculatePaymentForHoursWithAnEndTimeBeforeMidnight(BabySitter babySitter) {
 		int hoursWorkdFromStartToEnd = convertFor24HourTime(babySitter.getEndTime(), babySitter.getStartTime());
 		int hoursWorkdFromBedToEndTime = convertFor24HourTime(babySitter.getEndTime(), babySitter.getBedTime());
-		int retVal = calculatePaymentFromStartToBedTime(babySitter)
+		int totalPaymentForHoursFromStartToBeforeMidnight = calculatePaymentFromStartToBedTime(babySitter)
 				+ PAY_RATE_BEDTIME_TO_MIDNIGHT * hoursWorkdFromBedToEndTime;
 
 		if (babySitter.getBedTime() == 0) {
 
-			retVal = (PAY_RATE_START_TO_BED_TIME * hoursWorkdFromStartToEnd);
+			totalPaymentForHoursFromStartToBeforeMidnight = (PAY_RATE_START_TO_BED_TIME * hoursWorkdFromStartToEnd);
 		}
-		return retVal;
+		return totalPaymentForHoursFromStartToBeforeMidnight;
 	}
 
 	private int calculatePaymentFromStartToBedTime(BabySitter babySitter) {
 		int hoursWorkedFromStartToBedTime = convertFor24HourTime(babySitter.getBedTime(), babySitter.getStartTime());
+		int totalPaymentForHoursFromStartToBedTime = (PAY_RATE_START_TO_BED_TIME * hoursWorkedFromStartToBedTime);
 
-		return (PAY_RATE_START_TO_BED_TIME * hoursWorkedFromStartToBedTime);
+		return totalPaymentForHoursFromStartToBedTime;
 
 	}
 
